c57ed966f5bef06ecf7904ebb116f73ddb08585f
Maria <mroblesar@gmail.com>
2018-04-14 22:38:45 +0200

    4.3: Controllers - Landing Page added, Orders Controll added

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  get 'simple_pages/products'
 
-  root 'products#index'
+  root 'simple_pages#landing_page'
+  # root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
