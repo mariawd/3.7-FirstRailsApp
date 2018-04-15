a19897d0db88c749449421a6444ece4abe848712
Maria <mroblesar@gmail.com>
2018-04-15 22:31:35 +0200

    Active Record Associations, Orders model created and related to product model

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/products'
+
   get 'simple_pages/landing_page'
+
+  get 'products/index'
 
   root 'simple_pages#landing_page'
   # root 'products#index'
