297bc6971c40dffc388f1503a0c5a9190782a8e5
Maria <mroblesar@gmail.com>
2018-04-18 01:40:27 +0200

    4.5: Views: Carousel added to landing_page, rendering partial views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,11 @@
 class SimplePagesController < ApplicationController
-  def index
-  end
-
   def landing_page
     @featured_product = Product.first
+    @products = Product.limit(3)
   end
+end
 
+class SimplePagesController < ApplicationController
+  def index
+  end
 end
