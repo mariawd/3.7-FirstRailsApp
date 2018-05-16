a4dbc9db2f88fa5c46705563757fd78c4c8f2d3f
Maria <mroblesar@gmail.com>
2018-05-16 23:30:51 +0200

    5.10 Add automated Email on User Sign Up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,6 +10,12 @@ class UserMailer < ApplicationMailer
          :subject => "A new contact form message from #{name}")
   end
 
+  def welcome(user)
+    @appname = "Bike Berlin"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
+  end
+
   def thank_you
     @name = params[:name]
     @email = params[:email]
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', controllers: { registrations: "user_registrations" }}
   resources :users
   resources :products do
-    resources :comments
+  resources :comments
   end
 
-  get '/products', to: 'products#index'
   get 'simple_pages/index'
-  get 'simple_pages/about'
   get 'simple_pages/contact'
-
   get 'simple_pages/landing_page'
 
-  get 'products/index'
-
   root 'simple_pages#landing_page'
-  #root 'simple_pages#index'
 
   post 'simple_pages/thank_you'
 
