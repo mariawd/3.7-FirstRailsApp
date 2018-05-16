56170e0b826aa2c2d6859a546b803f05bbb4a717
Maria <mroblesar@gmail.com>
2018-05-16 23:46:50 +0200

    Correct route errors

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
   get 'simple_pages/contact'
+  get 'simple_pages/about'
   get 'simple_pages/landing_page'
 
   root 'simple_pages#landing_page'
