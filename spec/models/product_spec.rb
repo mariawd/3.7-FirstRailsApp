78128f1ecd778d229fadc0ad26900bae8b46ba3e
Maria <mroblesar@gmail.com>
2018-05-20 18:00:50 +0200

    6:2 Create spec files and write test for average_rating method

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Product do
+  let(:product) { Product.create!(name: "race bike") }
+  let (:user) {User.create!(first_name: "Maria", last_name:"Robles", email: "mariarobleshottel@gmail.com", password: "mari08")}
+
+  before do
+    product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+    product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+    product.comments.create!(rating: 5, user: user, body: "Great bike!")
+  end
+
+  it "returns the average rating of all comments" do
+   expect(@product.average_rating).to eq 3
+  end
+
+  it "is not valid without a name" do
+    expect(Product.new(description: "Nice Bike")).not_to be_valid
+  end
+
+end
