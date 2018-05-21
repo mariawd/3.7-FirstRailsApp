382944deebf4ca40c1ca82a269c1c2c73bf4865e
Maria <mroblesar@gmail.com>
2018-05-21 23:35:32 +0200

    Fix error in average rating test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,13 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all.paginate(page: params[:page], per_page: 5)
-    if params[:q]
+      if params[:q]
       search_term = params[:q]
       @products = Product.search(search_term)
     else
       @products = Product.all
     end
+      @products = @products.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /products/1
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :product
 
   validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
 
   scope :rating_desc, -> { order(rating: :desc) }
