80205fd0f0e4dcd38738120ee862c66ccb3f21f7
Maria <mroblesar@gmail.com>
2018-05-23 15:33:41 +0200

    6.3: Write Functional Test for User Authentication login

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -12,13 +12,9 @@ class SimplePagesController < ApplicationController
   end
 
   def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    ActionMailer::Base.mail(:from => @email,
-      :to => 'mroar2003@yahoo.com.ar',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-  end
-
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      UserMailer.contact_form(@email, @name, @message).deliver_now
+      end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,12 +16,4 @@ class UserMailer < ApplicationMailer
          subject: "Welcome to #{@appname}!")
   end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
-
-
 end
