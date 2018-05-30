7eb45d42210997f0430547d2952d1cddb440363d
Maria <mroblesar@gmail.com>
2018-05-30 23:19:48 +0200

    Fix price als decimal

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,7 @@ ActiveRecord::Schema.define(version: 20180511090258) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.string   "price"
-    t.string   "decimal"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|
