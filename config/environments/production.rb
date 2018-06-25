0674bbf9eaaaab29138bd84530fd6333167a63f7
Maria <mroblesar@gmail.com>
2018-06-25 09:40:45 +0200

    6.13 Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_socket_server_url = "wss://https://bikeberlinrails.herokuapp.com//cable"
+  config.web_socket_server_url = "wss://bikeberlinrails.herokuapp.com//cable"
   config.action_cable.allowed_request_origins = ['https://bikeberlinrails.herokuapp.com/', 'https://bikeberlinrails.herokuapp.com/']
 
 end
