09da758eedfd17a67d9a236fa2ae252119731ef3
Maria <mroblesar@gmail.com>
2018-06-27 11:49:59 +0200

    6.15 Tests and controls

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :product
 
   validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
 
   after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
