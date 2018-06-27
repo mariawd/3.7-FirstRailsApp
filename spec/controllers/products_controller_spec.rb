1f6bc641214dc5567d47f41d65bbaf515109287f
Maria <mroblesar@gmail.com>
2018-06-27 12:40:19 +0200

    6.15 Tests and controls

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+  describe ProductsController, type: :controller do
+
+    context "GET #index" do
+      it 'renders the index page' do
+        get :index
+        expect(response).to be_ok
+        expect(response).to render_template('index')
+      end
+    end
+
+    context "GET #new" do
+      it 'renders the new page' do
+        get :new
+        expect(response).to be_ok
+        expect(response).to render_template('new')
+      end
+    end
+
+    context "GET #edit" do
+      before do
+        @product = Product.create(name: "Testing Bike Berlin", description: "test", colour: "red", price: 100)
+      end
+    end
+
+    context "GET #show" do
+      it 'renders the show page' do
+      get :show, params: {id: product.id}
+      expect(response).to be_ok
+      expect(response).to render_template('show')
+    end
+
+    context "POST #create" do
+    before do
+      @user = FactoryBot.create(:user)
+      end
+    end
+
+    context "POST #update" do
+
+      before do
+        @product = Product.create(name: "Testing Bike Berlin", description: "test", colour: "red", price: 100)
+      end
+    end
+
+    context "DELETE #destroy" do
+      before do
+        @product = Product.create(name: "Test", description: "test", colour: "red", price: 100)
+      end
+    end
+end
